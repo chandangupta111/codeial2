005beaadcdee054c538aef84fc3b0bc3eb27ac37
Chandan Kumar <chandanguptabasadhi318@gmail.com>
2023-08-08 10:37:29 -0700

    uplode image

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -12,15 +12,45 @@ module.exports.profile = function(req, res){
 }
 
 
-module.exports.update = function(req, res){
+module.exports.update = async function(req, res){
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //         req.flash('success', 'Updated!');
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     req.flash('error', 'Unauthorized!');
+    //     return res.status(401).send('Unauthorized');
+    // }
+
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-            req.flash('success', 'Updated!');
+        try{
+            // console.log("chandan");
+
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req,res,function(err){
+                if(err) {console.log('*****Multer Error:', err)}
+                user.name = req.body.name;
+                user.email = req.body.email
+                 console.log(req.body);
+
+            if(req.file){
+                user.avatar =User.avatarPath +req.file.filename
+            }
+            user.save();
+            return res.redirect('back')
+            });
+
+        }catch(err){
+            req.flash('error', err);
             return res.redirect('back');
-        });
+        }
+
+        
     }else{
         req.flash('error', 'Unauthorized!');
-        return res.status(401).send('Unauthorized');
+       return res.status(401).send('Unauthorized');
+
     }
 }
 
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const multer = require('multer')
 const path = require('path');
-const AVATAR_PATH =path.join('/uplodes/users/avatars');
+const AVATAR_PATH = path.join('/uplodes/users/avatars');
 
 
 const userSchema = new mongoose.Schema({
@@ -18,10 +18,30 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String,
+        require: true
     }
 }, {
     timestamps: true
 });
+
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + DataTransfer.now());
+    }
+  });
+
+  //static function
+
+  userSchema.statics.uploadeAvatar = multer({storage: storage}).single('avatar');
+  userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User', userSchema);
 
